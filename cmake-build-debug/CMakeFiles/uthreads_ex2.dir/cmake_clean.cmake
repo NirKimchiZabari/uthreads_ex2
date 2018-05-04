31ee0a98c61249b817c2c65f223f99a9eef4cbe9
nir.zabari <nir.zabari@mail.huji.ac.il>
2018-05-04 13:41:24 +0300

    initial commit

---
+++ cmake-build-debug/CMakeFiles/uthreads_ex2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/uthreads_ex2.dir/main.cpp.o"
+  "CMakeFiles/uthreads_ex2.dir/uthreads.cpp.o"
+  "uthreads_ex2.pdb"
+  "uthreads_ex2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/uthreads_ex2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
